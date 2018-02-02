23c318ed670fd8a8d61e064a9704dde25d39d92a
Sebastian <sabadus_sebastian@yahoo.com>
2018-02-02 10:02:56 +0200

    DB user and pass added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-const MONGODB_URI = 'ds123258.mlab.com:23258/todoapp-api';
+const MONGODB_URI = 'mongodb://admin:admin123@ds123258.mlab.com:23258/todoapp-api';
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
