798ef860aa7f8940aad3f176b37974536fd595d2
Sebastian <sabadus_sebastian@yahoo.com>
2018-02-04 18:36:11 +0200

    Hash user pas before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,32 @@
 const {SHA256} = require('crypto-js');
 //Jason web token used for autentication
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 
-var data = {
-  id: 5
-};
-var token = jwt.sign(data, 'abc123');
-console.log(token);
+var password = '123abc!';
 
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, 'abc123');
-console.log('Decoded', decoded);
+var hashedPass = '$2a$10$KclTVrqdJsC.AtsUyNzMMePqyhMNxJjzk1ab.CpiNRIqLK6AqO34q';
+
+bcrypt.compare(password, hashedPass, (err, res) => {
+  console.log(res);
+});
+//
+// var data = {
+//   id: 5
+// };
+// var token = jwt.sign(data, 'abc123');
+// console.log(token);
+//
+//
+// var decoded = jwt.verify(token, 'abc123');
+// console.log('Decoded', decoded);
 //Using raw SHA encription
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
