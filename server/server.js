bafe8f6455fb0295cef0d8cc6381fdd02e0d9bca
Sebastian <sabadus_sebastian@yahoo.com>
2018-02-04 18:02:05 +0200

    Add GET /users/me and autenticate

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {autenticate} = require('./middleware/autenticate.js');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -102,6 +103,10 @@ app.post('/users', (req, res) => {
  }).then((token) => {
    res.header('x-auth', token).send(user);
  }).catch((err) => res.status(400).send(err));
+});
+
+app.get('/users/me', autenticate,(req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
