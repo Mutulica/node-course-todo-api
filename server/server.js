6b8161a252870e9f917c1604f147ee525f55e0da
Sebastian <sabadus_sebastian@yahoo.com>
2018-02-05 16:28:59 +0200

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -122,6 +122,12 @@ app.post('/users/login', (req, res) => {
   });
 });
 
+app.delete('/users/me/token', autenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }).catch((err) => res.status(400).send());
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
